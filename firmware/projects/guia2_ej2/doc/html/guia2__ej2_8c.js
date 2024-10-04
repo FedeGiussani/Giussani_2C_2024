0df588715784f2964deedecf39a4dc0e64f59d86
FedeGiussani <federicogiussani.2001@gmail.com>
2024-10-04 14:44:29 -0300

    Documentacion_ej02_guia02

---
+++ firmware/projects/guia2_ej2/doc/html/guia2__ej2_8c.js
@@ -0,0 +1,17 @@
+var guia2__ej2_8c =
+[
+    [ "REFRESCO_DISPLAY", "guia2__ej2_8c.html#a9354176bdb497f58db3866aa902e2bcc", null ],
+    [ "REFRESCO_MEDICION", "guia2__ej2_8c.html#a55c44d1988adb48bb8b4731ecd8e9e36", null ],
+    [ "app_main", "guia2__ej2_8c.html#a630544a7f0a2cc40d8a7fefab7e2fe70", null ],
+    [ "FuncTimerMedir", "guia2__ej2_8c.html#a33cc99a5f431a4c678bc1d9e2c7892c4", null ],
+    [ "FuncTimerMostrar", "guia2__ej2_8c.html#aee67be6cf701ffd6dd57f89bb842d026", null ],
+    [ "medirTask", "guia2__ej2_8c.html#a5e3b530b57e397f13f97c14f6d1a5078", null ],
+    [ "mostrarTask", "guia2__ej2_8c.html#abafd14d719f27a0ecd842a8d6c7cb662", null ],
+    [ "TeclaHold", "guia2__ej2_8c.html#ad9f5552a6ceb69c4a0f1ec0e3f5461f0", null ],
+    [ "TeclaOn", "guia2__ej2_8c.html#a52fbc45ed4e53c69959b35c1978cf90f", null ],
+    [ "distancia", "guia2__ej2_8c.html#aec40b15ff3098bdb2425eb4dc9c11a18", null ],
+    [ "hold", "guia2__ej2_8c.html#af0129e079b57e897edf00f22cc25f3d4", null ],
+    [ "Medir_task_handle", "guia2__ej2_8c.html#ac775c51d06b43d7dcf5458c6864f9577", null ],
+    [ "Mostrar_task_handle", "guia2__ej2_8c.html#a2697c9427db0ca0ce43f563949c84da9", null ],
+    [ "on", "guia2__ej2_8c.html#aaa928c9a62449f7946da1e32f66c70d2", null ]
+];
--- firmware/projects/guia2_ej2/main/guia2_ej2.c
+++ firmware/projects/guia2_ej2/main/guia2_ej2.c
@@ -2,12 +2,10 @@
  *
  * \section genDesc General Description
  * 
- * @section hardConn Hardware Connection
- *
- * |    Peripheral  |   ESP32   	|
- * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
- *
+ * Aplicación que mide distancias utilizando un sensor ultrasónico, muestra los resultados en un display LCD, 
+ * y controla un conjunto de LEDs basándose en las mediciones obtenidas. El sistema también responde a entradas 
+ * de usuario a través de teclas para encender/apagar el dispositivo y mantener la última medición en pantalla.
+ * 
  * @section changelog Changelog
  *
  * |   Date	    | Description         |
@@ -30,30 +28,83 @@
 #include "switch.h"
 #include "timer_mcu.h"
 /*==================[macros and definitions]=================================*/
+/**
+ * @def REFRESCO_MEDICION
+ * @brief Intervalo de refresco para la tarea de medición de distancia (en ms).
+ */
 #define REFRESCO_MEDICION 1000000
 
+/**
+ * @def REFRESCO_DISPLAY
+ * @brief Intervalo de refresco para la tarea de actualización del display (en ms).
+ */
 #define REFRESCO_DISPLAY 100000
 /*==================[internal data definition]===============================*/
+/**
+ * @brief Handle para la tarea de medición de distancia.
+ */
 TaskHandle_t Medir_task_handle = NULL;
 
+/**
+ * @brief Handle para la tarea de mostrar la distancia y controlar LEDs.
+ */
 TaskHandle_t Mostrar_task_handle = NULL;
 
+/**
+ * @brief Variable para almacenar la distancia medida por el sensor ultrasónico (en cm).
+ */
 uint16_t distancia = 0;
 
+/**
+ * @brief Estado que indica si el sistema está en modo "hold" (mantiene la distancia en pantalla).
+ */
 bool hold;
 
+/**
+ * @brief Estado del sistema, encendido o apagado.
+ */
 bool on;
+
 /*==================[internal functions declaration]=========================*/
+/**
+ * @fn void FuncTimerMedir(void *param)
+ * @brief Función llamada por un temporizador para notificar a la tarea de medición de distancia.
+ * 
+ * Envía una notificación a la tarea encargada de realizar la medición de distancia para que se ejecute.
+ * 
+ * @param param Parámetro no utilizado.
+ * @return 
+ */
 void FuncTimerMedir(void *param)
 {
     vTaskNotifyGiveFromISR(Medir_task_handle, pdFALSE); /* Envía una notificación a la tarea asociada a medir*/
 }
 
+/**
+ * @fn void FuncTimerMostrar(void *param)
+ * @brief Función llamada por un temporizador para notificar a la tarea de mostrar la distancia y controlar LEDs.
+ * 
+ * Envía una notificación a la tarea encargada de mostrar la distancia y controlar los LEDs.
+ * 
+ * @param param Parámetro no utilizado.
+ * @return 
+ */
 void FuncTimerMostrar(void *param)
 {
     vTaskNotifyGiveFromISR(Mostrar_task_handle, pdFALSE); /* Envía una notificación a la tarea asociada al mostrar */
 }
 
+/**
+ * @fn void medirTask()
+ * @brief Tarea para medir la distancia utilizando el sensor ultrasónico.
+ * 
+ * La tarea se ejecuta tras recibir una notificación (mediante `ulTaskNotifyTake`), 
+ * y cuando el sistema está encendido (`on`), mide la distancia con el sensor ultrasónico.
+ * El resultado se almacena en la variable `distancia`.
+ * 
+ * @note La tarea espera indefinidamente hasta recibir la notificación para ejecutarse.
+ * @return 
+ */
 void medirTask()
 { // en esta tarea se mide las distancias por el sensor y se modifica el estado de los leds dependiendo lo que se este midiendo
     while (true)
@@ -67,6 +118,22 @@ void medirTask()
     }
 }
 
+/**
+ * @fn void mostrarTask()
+ * @brief Tarea para mostrar la distancia en el display y controlar los LEDs.
+ * 
+ * La tarea se ejecuta tras recibir una notificación (mediante `ulTaskNotifyTake`), 
+ * y cuando el sistema está encendido (`on`), enciende o apaga los LEDs en función de la distancia medida 
+ * y actualiza la pantalla LCD a menos que esté en modo "hold".
+ * 
+ * - Distancia < 10 cm: Apaga todos los LEDs.
+ * - Distancia entre 10 y 20 cm: Enciende solo LED_1.
+ * - Distancia entre 20 y 30 cm: Enciende LED_1 y LED_2.
+ * - Distancia > 30 cm: Enciende todos los LEDs.
+ * 
+ * @note La tarea espera indefinidamente hasta recibir la notificación para ejecutarse.
+ * @return 
+ */
 void mostrarTask()
 {
     while (true)
@@ -112,11 +179,26 @@ void mostrarTask()
     }
 }
 
+/**
+ * @fn void TeclaOn()
+ * @brief Función que alterna el estado de encendido/apagado del sistema.
+ * 
+ * Esta función es llamada cuando el usuario presiona el botón para encender o apagar el sistema.
+ * @return
+ */
 void TeclaOn()
 {
     on = !on;
 }
 
+/**
+ * @fn void TeclaHold()
+ * @brief Función que alterna el modo "hold" (mantener la distancia en pantalla) del sistema.
+ * 
+ * Esta función es llamada cuando el sistema está encendido (`on`) y el usuario presiona el botón para activar 
+ * o desactivar el modo "hold".
+ * @return 
+ */
 void TeclaHold()
 {
     if (on)
